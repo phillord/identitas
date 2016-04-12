7846922756976b6b56403b1bda094aa5fc8a43e7
Phillip Lord <phillip.lord@newcastle.ac.uk>
2016-04-12 15:14:30 +0100

    Fix short Tests.

--- test/identitas/proquint_test.clj
+++ test/identitas/proquint_test.clj
@@ -22,15 +22,15 @@
 (deftest short-proshort-conversions
   (is
    (=
-    ["babab" "babab-babab" "luzuz-zuzuz"]
-    (map p/int-to-proint [Integer/MIN_VALUE 0 Integer/MAX_VALUE])))
+    ["babab" "zuzuz"]
+    (map p/short-to-proshort [0 65535])))
 
   (is
    (thrown?
     IllegalArgumentException
-    (p/int-to-proint (+ 1 Integer/MAX_VALUE))))
+    (p/short-to-proshort 65536)))
 
   (is
    (thrown?
     IllegalArgumentException
-    (p/int-to-proint (- 1 Integer/MIN_VALUE)))))
+    (p/short-to-proshort -1))))
