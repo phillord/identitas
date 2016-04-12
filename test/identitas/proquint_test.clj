aecdcf48398007cf5c112155989e3ee37adab842
Phillip Lord <phillip.lord@newcastle.ac.uk>
2016-04-12 14:52:41 +0100

    Tests for proquint.

---
+++ test/identitas/proquint_test.clj
@@ -0,0 +1,20 @@
+(ns identitas.proquint-test
+  (:require [identitas.proquint :as p]
+            [clojure.test :refer :all]))
+
+
+(deftest int-proint-conversions
+  (is
+   (=
+    ["mabab-babab" "babab-babab" "luzuz-zuzuz"]
+    (map p/int-to-proint [Integer/MIN_VALUE 0 Integer/MAX_VALUE])))
+
+  (is
+   (thrown?
+    IllegalArgumentException
+    (p/int-to-proint (+ 1 Integer/MAX_VALUE))))
+
+  (is
+   (thrown?
+    IllegalArgumentException
+    (p/int-to-proint (- 1 Integer/MIN_VALUE)))))
