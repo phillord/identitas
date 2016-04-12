0cde6cf5a7fbfce6c03d26fcadd31b182031fabe
Phillip Lord <phillip.lord@newcastle.ac.uk>
2016-04-12 14:52:00 +0100

    Long Conversion tests.

--- test/identitas/util_test.clj
+++ test/identitas/util_test.clj
@@ -13,3 +13,16 @@
 
   (is (= [[-32768 0] [32767 -1]]
          (map #(u/integer-to-short %) [Integer/MIN_VALUE Integer/MAX_VALUE]))))1
+
+
+(deftest integer-long-conversions
+  (is
+   (= [[0 0] [0 Integer/MAX_VALUE]]
+      (map #(u/long-to-integer %) [0 Integer/MAX_VALUE])))
+  (is
+   (=
+    [0 Integer/MAX_VALUE]
+    (map #(u/integer-to-long %) [[0 0] [0 Integer/MAX_VALUE]])))
+
+  (is (= [[-140737488355328 0] [140737488355327 -1]]
+         (map #(u/integer-to-short %) [Long/MIN_VALUE Long/MAX_VALUE]))))
