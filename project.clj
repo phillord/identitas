2f29772ede29f39b652e0269377ed15cf4083947
Nizal Alshammry <nizalalshammry@newhuaweiapstaff-10-51-0-139.ncl.ac.uk>
2017-08-24 18:17:48 +0100

    Update Clojure version to use includes?
    clojure.string/includes? not exist in version 1.7.0

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject identitas "0.1.0-SNAPSHOT"
   :description "Mechanisms for generating and manipulating identifiers."
   ;;:url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [primitive-operator "0.2-SNAPSHOT"]])
